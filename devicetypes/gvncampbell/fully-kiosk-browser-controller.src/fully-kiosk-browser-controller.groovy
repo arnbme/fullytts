e62ddac6ca00e695e181a903a608baa7c78753ab
Arn Burkhoff <arnb@arnb.org>
2019-03-27 17:44:57 -0400

    add functions and tiles

--- devicetypes/gvncampbell/fully-kiosk-browser-controller.src/fully-kiosk-browser-controller.groovy
+++ devicetypes/gvncampbell/fully-kiosk-browser-controller.src/fully-kiosk-browser-controller.groovy
@@ -11,13 +11,17 @@
  *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
  *  for the specific language governing permissions and limitations under the License.
  *
- *	Mar 25, 2019       Arn Burkhoff: Add some really messed up Tiles code, it mostly works.
+ *	Mar 27, 2019       known issue SmartThings: Sliders don't update real time, and TTS Sound fails to adjust volume
+ *	Mar 26, 2019       Arn Burkhoff: ST V2.02 same code runs on Smarthings and Fully
+ *	Mar 25, 2019       Arn Burkhoff: ST V2.01 add Capability Alarm, alarm file and commands off, both, alarm
+ *												playSound, stopSound. Comment out mute and unmute
+ *	Mar 25, 2019       Arn Burkhoff: ST V2.00 Add some really messed up Tiles code, it mostly works.
  *										note keep sendEvent before the sendCommand slider does not impact remotte device
- *	Mar 24, 2019 	   Arn Burkhoff: Compatability with SmartThings and Hubitat in a single module instance
+ *	Mar 24, 2019 	   Arn Burkhoff: ST V2.00 Compatability with SmartThings and Hubitat in a single module instance
  *	Mar 24, 2019 V1.05 Gavin Campbell: Update to 1.05 Hubitat version level. Add setScreenBrightness
  *	Mar 23, 2019 V1.04 Gavin Campbell: Update to 1.04 Hubitat version level.
- *	Mar 22, 2019 	   Arn Burkhoff: Add chime command giving partial Lannouncer compatability.
- *  Mar 22, 2019 	   Arn Burkhoff: Port to Smarthings from Hubitat
+ *	Mar 22, 2019 	   Arn Burkhoff: ST V1.00 Add chime command giving partial Lannouncer compatability.
+ *  Mar 22, 2019 	   Arn Burkhoff: ST V1.00 Port to Smarthings from Hubitat
  *	Mar 21, 2019 V1.00 Gavin Campbell: Released on Hubitat
  */
 
@@ -33,6 +37,9 @@ metadata {
 		capability "AudioVolume"
         capability "Refresh"
 		capability "Actuator"
+		capability "Alarm"
+		if (isSmartThings())
+			attribute "volume","number"
 		command "launchAppPackage"
 		command "bringFullyToFront"
 		command "screenOn"
@@ -43,13 +50,18 @@ metadata {
 		command "loadURL",["String"]
 		command "loadStartURL"
 		command "setScreenBrightness",["Number"]
-        command "chime"
+		command "chime"
+		command "alarm"
+		command "playSound",["String"]
+		command "stopSound"
+
     }
 	preferences {
 		input(name:"serverIP",type:"string",title:"Server IP Address",defaultValue:"",required:true)
 		input(name:"serverPort",type:"string",title:"Server Port",defaultValue:"2323",required:true)
 		input(name:"serverPassword",type:"string",title:"Server Password",defaultValue:"",required:true)
 		input(name:"toneFile",type:"string",title:"Tone Audio File URL",defaultValue:"",required:false)
+		input(name:"alarmFile",type:"string",title:"Alarm Audio File URL",defaultValue:"",required:false)
 		input(name:"appPackage",type:"string",title:"Application to Launch",defaultValue:"",required:false)
 		input(name:"loggingLevel",type:"enum",title:"Logging Level",description:"Set the level of logging.",options:["none","debug","trace","info","warn","error"],defaultValue:"debug",required:true)
     }
@@ -59,7 +71,7 @@ metadata {
 			{
 			standardTile("screenOn", "device.switch", inactiveLabel: false, decoration: "flat") 
 				{
-				state "default", label:'Screen On', action:"screenOn", backgroundColor: "#00a0dc"
+				state "default", label:'Screen On', action:"screenOn"
 				}
 			controlTile("levelSliderControl", "device.levelx", "slider", height: 1,
 	             width: 1, inactiveLabel: false, range:"(0..255)")
@@ -92,7 +104,7 @@ metadata {
 				}
 			standardTile("saverOn", "device.switch", inactiveLabel: false, decoration: "flat") 
 				{
-				state "default", label:'Screen Saver On', action:"startScreensaver", backgroundColor: "#00a0dc"
+				state "default", label:'Screen Saver On', action:"startScreensaver"
 				}
 			standardTile("saverOff", "device.switch", inactiveLabel: false, decoration: "flat") 
 				{
@@ -106,14 +118,14 @@ metadata {
 				{
 				state "default", label:'Load Start URL', action:"loadStartURL"
 				}
-			standardTile("Mute", "device.switch", inactiveLabel: false, decoration: "flat") 
-				{
-				state "default", label:'Mute', action:"mute"
-				}
-			standardTile("Unmute", "device.switch", inactiveLabel: false, decoration: "flat") 
-				{
-				state "default", label:'UnMute', action:"unmute"
-				}
+//			standardTile("mute", "device.switch", inactiveLabel: false, decoration: "flat") 
+//				{
+//				state "default", label:'Mute Sound', action:"mute"
+//				}
+//			standardTile("Unmute", "device.speech", inactiveLabel: false, decoration: "flat") 
+//				{
+//				state "default", label:'UnMute', action:"unmute"
+//				}
 			standardTile("volumeup", "device.speech", inactiveLabel: false, decoration: "flat") 
 				{
 				state "default", label:'Volume up 10%', action:"volumeUp"
@@ -121,13 +133,25 @@ metadata {
 			controlTile("volumeSliderControl", "device.volume", "slider", height: 1,
 	             width: 1, inactiveLabel: false, range:"(0..100)")
 	            {
-			    state "volume", action:"setVolume", label:"${currentValue}"
+			    state "volume", action:"setVolume", label:'${currentValue}'
 				}
 			standardTile("volumedown", "device.speech", inactiveLabel: false, decoration: "flat") 
 				{
 				state "default", label:'Volume down 10%', action:"volumeDown"
 				}
-			standardTile("refresh", "device.speech", inactiveLabel: false, decoration: "flat") 
+			standardTile("soundalarm", "device.alarm", inactiveLabel: false, decoration: "flat") 
+				{
+				state "default", label:'Sound Alarm', action:"siren"
+				}
+			standardTile("playsound", "device.tone", inactiveLabel: false, decoration: "flat") 
+				{
+				state "default", label:'Play Sound', action:'playSound'
+				}
+			standardTile("stopsound", "device.tone", inactiveLabel: false, decoration: "flat") 
+				{
+				state "default", label:'Stop Sound', action:'stopSound'
+				}
+			standardTile("refresh", "device.tone", inactiveLabel: false, decoration: "flat") 
 				{
 				state "default", label:'Screen Refresh', action:"refresh"
 				}
@@ -137,17 +161,24 @@ metadata {
 	}
 
 def parse(String description) {
-	if (isSmartThings)
+	if (isSmartThings())
 		{	
 		def logprefix = "[parse]"
 		logger(logprefix+' '+description,"trace")
 		def map
 		map = stringToMap(description)
 		def headerString = new String(map.headers.decodeBase64())
+		def bodyString = new String(map.body.decodeBase64())
+		log
 		logger(logprefix+' headers:'+headerString,'trace')
-		if (!headerString.contains("200 OK")) 
+		if (headerString.contains("200 OK"))
 			{
-			def bodyString = new String(map.body.decodeBase64())
+			if (bodyString.contains("Error"))
+				logger (logprefix+' '+bodyString,'error')  
+			}
+
+		else
+			{
 			logger (logprefix+' '+headerString+' '+bodyString,'error')  
 			}
 		}	
@@ -238,10 +269,28 @@ def setVolume(volumeLevel) {
 	def logprefix = "[setVolume] "
 	logger(logprefix+"volumeLevel:${volumeLevel}")
 	sendEvent([name:"volume",value:volumeLevel])
-	for (i=1;i<=10;i++) {
-		sendCommandPost("cmd=setAudioVolume&level=${volumeLevel}&stream=${i}")
+//	for (def i=1;i<=10;i++) {
+//		sendCommandPost("cmd=setAudioVolume&level=${volumeLevel}&stream=${i}")
+//	}
+	sendCommandPost("cmd=setAudioVolume&level=${volumeLevel}&stream=2")	//MP3 Sound stream
+	if (isSmartThings())
+		{
+		def now = new Date()
+		def runTime = new Date(now.getTime() + 3000)
+		runOnce(runTime, setVolumeDelayed9,[data: [volume: volumeLevel]])
+		}
+	else
+		{
+		sendCommandPost("cmd=setAudioVolume&level=${volumeLevel}&stream=9")  //TTS stream
+		}
 	}
-}
+	
+def setVolumeDelayed9(evt)
+	{
+	def logprefix = "[setVolumeDelayed2] + ${evt}" 
+	logger(logprefix)
+	sendCommandPost("cmd=setAudioVolume&level=${evt.volume}&stream=9") //MP3 Sound stream 
+	}
 def volumeUp() {
 	def logprefix = "[volumeUp] "
 	logger(logprefix)
@@ -262,19 +311,41 @@ def volumeDown() {
 		setVolume(newVolume)
 	}
 }
-def mute() {
-	def logprefix = "[mute] "
-	logger(logprefix)
-}
-def unmute() {
-	def logprefix = "[unmute] "
-	logger(logprefix)
-}
 def refresh() {
   	def logprefix = "[refresh] "
   	logger logprefix
 	sendCommandPost("cmd=deviceInfo")
 }
+// *** [Alarm commands]********************************************************
+def off()		//alias for stopSound
+	{stopSound()}
+def both()		//alias for alarm
+	{alarm()}
+def siren()		//alias for alarm
+	{alarm()}
+def alarm()
+	{
+	def logprefix = "[alarm] "
+    logger(logprefix,"trace")
+    setVolume(100)
+	sendCommandPost("cmd=playSound&url=${alarmFile}&loop=true")
+	}
+// *** [Play Stop any sound file command]*******************************************
+def playSound(SoundFile="")
+	{
+	def logprefix = "[playSound] "
+    logger(logprefix,"trace")
+    if (SoundFile>"")
+		sendCommandPost("cmd=playSound&url=${SoundFile}")
+	else 
+		sendCommandPost("cmd=playSound&url=${toneFile}")
+	}
+def stopSound()
+	{
+	def logprefix = "[stopSound] "
+    logger(logprefix,"trace")
+	sendCommandPost("cmd=stopSound")
+	}
 
 // *** [ Platform Determination Methods ] *************************************
 private isSmartThings()
